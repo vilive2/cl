19798807b787701537a02e04cc22dce5a92f6f85
vivek kumar <vivekxkr@gmail.com>
2024-11-03 21:12:42 +0530

    dfs algo

--- algorithms/graph/dfs.c
+++ algorithms/graph/dfs.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 #include "../../data-structures/list/list.h"
-#include "../../data-structures/queue/queue.h"
+#include "../../data-structures/stack/stack.h"
 #define N 1000
 
 LIST adj[N];
 int n;
 char vis[N];
 
-void bfs(int s) {
+void dfs(int s) {
     memset(vis, 0, n);
 
-    QUEUE q;
-    queue_init(&q, sizeof(int), n);
-    queue_push_back(&q, &s);
+    STACK st;
+    stack_init(&st, sizeof(int), n);
+    stack_push(&st, &s);
     vis[s] = 1;
 
-    while(q.size > 0) {
+    while(st.size > 0) {
         int u;
-        queue_pop_front(&q, &u);
+        stack_pop(&st, &u);
 
         for(int i = 0 ; i < adj[u].size ; i++) {
             int v;
             list_get(&(adj[u]), &v, i);
             if(vis[v] == 0) {
                 vis[v] = 1;
-                queue_push_back(&q, &v);
+                stack_push(&st, &v);
             }
         }
 
@@ -53,7 +53,7 @@ int main() {
         list_append(&adj[v], &u);
     }
 
-    bfs(0);
+    dfs(0);
 
     return 0;
 }
